bff7713c7b6bacdbb41307c389cea790b0d388a4
sumitnair26 <sumit.nair26@gmail.com>
2024-05-27 17:06:35 +0530

    adding  schema

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@
 import { z } from "../node_modules/@hono/zod-openapi/dist/index";
 
 //Inputs from the users on the route
-
 const PramsSchema = z.object({
   id:z.string().min(1).max(10).openapi({
     param:{
@@ -10,6 +9,18 @@ const PramsSchema = z.object({
       in:"path"
     },
     example: "123"
+  })
+})
+
+const UserSchema = z.object({
+  name: z.string().min(1).max(10).openapi({
+    example:"Sumit"
+  }),
+  age: z.number().int().openapi({
+    example:30
+  }),
+  id: z.string().min(1).max(10).openapi({
+    example:"24"
   })
 })
 
