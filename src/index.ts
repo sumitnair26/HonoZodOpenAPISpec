5332932828dc321b60395b16645672fb6c54b0e6
sumitnair26 <sumit.nair26@gmail.com>
2024-05-27 17:22:05 +0530

    Added route

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
+import { createRoute } from '../node_modules/@hono/zod-openapi/dist/index';
+import { ParamsSchema } from './input';
+import { UserSchema } from './ouput';
 
+const getUserRoute = createRoute({
+  method: 'get',
+  path:'/user/{id}',
+  request:{
+    params: ParamsSchema
+  },
+  responses:{
+    200:{
+      content:{
+        'application/json':{
+          schema: UserSchema
+        }
+      },
+      description: "Get the users details"
+    }
+  }
+})
 
 
 
